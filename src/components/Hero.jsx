2afd274f5ba1c0bee545d4c0d4ee7bdaea0d701d
Reema_abde <Reemabdella702@gmail.com>
2025-07-11 22:34:39 +0300

    Added images for the hero sections

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+const images = [
+    '/public/images/meskott-hero1.jpeg',
+    '/public/images/messkott-hero2.jpeg',
+    '/public/images/meskott-hero3.jpeg',
+    '/public/images/photo-1414235077428-338989a2e8c0.avif',
+  ];
+
+export default function Hero() {
+    return(
+
+    )
+}
