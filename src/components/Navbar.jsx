016958607d2b057e3c529a341914425cc3dda7a0
Reema_abde <Reemabdella702@gmail.com>
2025-07-11 16:11:52 +0300

    Set up Nav sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,12 @@ useEffect(() => {
 
   return(
     <header className='sticky top-0 z-50 bg-[#0B1517] dark:bg-gray-900 shadow-md'>
-        <nav className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center"></nav>
+        <nav className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+            <ul className='flex items-center space-x-4 gap-6 text-white relative'>
+                <li><a href="#home" className='hover:text-pink-400'>Home</a></li>
+                <li><a href="#feedback" className='hover:text-pink-400'>About</a>Feedback</li>
+            </ul>
+        </nav>
     </header>
 )
 }
