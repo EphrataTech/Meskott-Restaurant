a3e3ecebe0be88493697385dd43b2859357d4e46
Ephrata <ephrata0327@gmail.com>
2025-07-11 10:36:06 +0300

    feat: add a signatures section and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
+import SignatureDishes from "./components/SignatureDishes"
+import "./index.css"
+
+
 
 export default function App() {
+    return <>
+      
+      <SignatureDishes/>
+     </>
 }
 
 
