634d1cee99dcdccb21036d7e5dfe086a83c7fd77
Ephrata <ephrata0327@gmail.com>
2025-07-11 12:13:27 +0300

    fix:fix some issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import MenuPage from "./components/MenuPage"
 import SignatureDishes from "./components/SignatureDishes"
+import ReservationForm from "./components/ReservationForm"
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignatureDishes />} />
         <Route path="/menu/:category" element={<MenuPage />} />
+        <Route path="/reservation" element={<ReservationForm />} />
       </Routes>
     </Router>
   )
