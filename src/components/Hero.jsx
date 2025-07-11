e99bdd749551f30d19dda112d962cf61aeb4e9a5
Reema_abde <Reemabdella702@gmail.com>
2025-07-11 22:37:44 +0300

    Set up state management using const and useState hook

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const images = [
     '/public/images/meskott-hero3.jpeg',
     '/public/images/photo-1414235077428-338989a2e8c0.avif',
   ];
-
+  const typeText = '🍽️ Welcome to Meskott Culinary Experience';
 export default function Hero() {
     return(
 
